584084d1be89391c75a1702e957d025dc2415d72
connor rigby <konnorrigby@gmail.com>
2018-07-13 09:23:38 -0700

    Add deps, publish to hex

--- mix.exs
+++ mix.exs
@@ -7,6 +7,10 @@ defmodule NetLogger.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      source_url: "https://github.com/connorrigby/janl",
+      homepage_url: "https://github.com/connorrigby/janl",
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -21,9 +25,24 @@ defmodule NetLogger.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      # only used for server.
+      {:esqlite, github: "connorRigby/esqlite", optional: true, only: :dev},
       {:jason, "~> 1.1"},
-      {:esqlite, github: "connorRigby/esqlite", optional: true},
-      {:uuid, "~> 1.1"}
+      {:uuid, "~> 1.1"},
+      {:ex_doc, "~> 0.18.3", only: :dev},
+      {:dialyxir, github: "jeremyjh/dialyxir", only: :dev}
     ]
   end
+
+  defp description,
+    do: """
+    Just another Network Logger
+    """
+
+  defp package,
+    do: [
+      licenses: ["MIT"],
+      maintainers: ["Connor Rigby"],
+      links: %{"GitHub" => "https://github.com/connorrigby/janl"}
+    ]
 end
